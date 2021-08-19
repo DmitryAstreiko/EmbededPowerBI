99c6a3e1caaa8b09f6e429f68910e01ea6e9f5c5
astreikodmitry <astreikodmitry@gmail.com>
2021-08-19 11:56:06 +0300

    Show reports and dashboard without getting token

--- App/src/PowerBiDashboardComponent.js
+++ App/src/PowerBiDashboardComponent.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from 'powerbi-client';
-import './PowerBI.css';
+//import "./App.css";
+
 
 //https://docs.microsoft.com/en-us/javascript/api/overview/powerbi/configure-report-settings
 
-export default class PowerBiComponent extends Component {
+export default class PowerBiDashboardComponent extends Component {
     render() {
         return (
             <div>
@@ -14,7 +15,7 @@ export default class PowerBiComponent extends Component {
                         embedConfig={{
                             type: this.props.typeEmbed,   // Supported types: report, dashboard, tile, visual and qna
                             id: this.props.reportId,
-                            embedUrl: `https://app.powerbi.com/reportEmbed?reportId=${this.props.reportId}&groupId=${this.props.groupId}&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVdFU1QtRVVST1BFLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwiY2VydGlmaWVkVGVsZW1ldHJ5RW1iZWQiOnRydWUsInVzYWdlTWV0cmljc1ZOZXh0Ijp0cnVlfX0%3d`,
+                            embedUrl: `https://app.powerbi.com/dashboardEmbed?dashboardId=${this.props.reportId}&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVdFU1QtRVVST1BFLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6ZmFsc2V9fQ%3d%3d`,
                             accessToken: this.props.defaultToken,
                             tokenType: models.TokenType.Aad,
                             pageName: this.props.defaultPage,
@@ -41,7 +42,7 @@ export default class PowerBiComponent extends Component {
                             ])
                         }
 
-                        cssClassName={"embed-container-report"}
+                        cssClassName={"embed-container-dashboard"}
 
                         getEmbeddedComponent={(embeddedReport) => {
                             window.report = embeddedReport;
--- App/src/pages/MainPage.tsx
+++ App/src/pages/MainPage.tsx
@@ -3,15 +3,18 @@ import css from './MainPage.module.scss';
 import { Panel, RichTextView, IconContainer } from '@epam/loveship';
 import { ReactComponent as UuiPromoImage } from '../icons/uui-promo-image.svg';
 import PowerBiComponent from '../PowerBiComponent';
-
+import PowerBiDashboardComponent from '../PowerBiDashboardComponent';
 
 export class MainPage extends React.Component {    
     render() {
         const reportId = '162ae399-1e14-4332-a519-746c57a3fd22';
         const groupId = '5d4e1aaf-dd67-47f1-9f66-392bd5be5803';
-        const typeEmbed = 'report';
-        const defaultPage = 'ReportSection171d5979945773c50493';
+        const typeReportEmbed = 'report';
+        const typeDashboardEmbed = 'dashboard';
+        const defaultPage1 = 'ReportSection30f5c6d000d026e4eeb7';
+        const defaultPage3 = 'ReportSection1483e8c382a0bad70b72';
         const defaultToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyIsImtpZCI6Im5PbzNaRHJPRFhFSzFqS1doWHNsSFJfS1hFZyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvYjQxYjcyZDAtNGU5Zi00YzI2LThhNjktZjk0OWYzNjdjOTFkLyIsImlhdCI6MTYyOTM1OTcwOSwibmJmIjoxNjI5MzU5NzA5LCJleHAiOjE2MjkzNjM2MDksImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVlFBcS84VEFBQUFQY25yZmVnS2REUFFVVHRxN2txWkhncUpjL3krQ0ZSR3ZDV3MwbUdiWndpVS9IZlhPbGR1MGltTk5zVEtmWjhzd0xlVzdoZmZwV0RQVVc2cmxFOGhjR0tqdTNOckVZUmFWM3pSWGRIVTZlbz0iLCJhbXIiOlsicnNhIiwibWZhIl0sImFwcGlkIjoiODcxYzAxMGYtNWU2MS00ZmIxLTgzYWMtOTg2MTBhN2U5MTEwIiwiYXBwaWRhY3IiOiIyIiwiZGV2aWNlaWQiOiJmMWUwMGI5Ni1hOWY3LTQzYzEtOGEzZC04NmYwZmQ5MDU0ODMiLCJmYW1pbHlfbmFtZSI6IkFzdHJlaWthIiwiZ2l2ZW5fbmFtZSI6IkR6bWl0cnkiLCJpcGFkZHIiOiIzNy4yMTQuNTcuMjIyIiwibmFtZSI6IkR6bWl0cnkgQXN0cmVpa2EiLCJvaWQiOiJiZTg2NmQzNi1jNjAwLTQ4NjAtOWQ3Ni1lYTZlMGNiOTQzNGMiLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtNjM2MTU3NC0yOTMwODI0MjItMTMwMDc2MTgtMTMxNzk1IiwicHVpZCI6IjEwMDMyMDAxM0IyRjkyMDciLCJyaCI6IjAuQVFrQTBISWJ0SjlPSmt5S2FmbEo4MmZKSFE4QkhJZGhYckZQZzZ5WVlRcC1rUkFKQU5jLiIsInNjcCI6InVzZXJfaW1wZXJzb25hdGlvbiIsInN1YiI6Iml4TDRMYWJsOEdCNlJtZmVKUHhFNnQwTzdTYWhBMEl0T1ZOQm9BVXJZb28iLCJ0aWQiOiJiNDFiNzJkMC00ZTlmLTRjMjYtOGE2OS1mOTQ5ZjM2N2M5MWQiLCJ1bmlxdWVfbmFtZSI6IkR6bWl0cnlfQXN0cmVpa2FAZXBhbS5jb20iLCJ1cG4iOiJEem1pdHJ5X0FzdHJlaWthQGVwYW0uY29tIiwidXRpIjoiSXdqN2xBcUYway1CY05XYUI1STJBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.FE_QHvJ6xpCyTWsttE7zBJfNPVo_n5GRhn4RRPq1dgq7RW5FzyyvY_UOaiaLfytGE8Mz8mO3sRHhWTxEdmIqob5swt5HXDFJMcpfrTI4_AvkSEZsRmqOy29wns7jOMNsYIjTfWfimiQI5wkhVMmNELNgzlxoe8Uh53m8tIwNaNMGGb-EfrEbTBuQphNN5dLWnLEkqX6QyHYcVp0cHCSbTrWpTcgwE2e6xUb8GSQmUoAbR0pnPd1Uj4axx-278ztc6E60uQ7KGHoic6CDd73PPXpHsle32J98yCAkUxG_NY1aMY8T8E7_P3g8T24z5uL6xoY4J0nd4wphk63BTpSvXg';
+        const DashboardId = '0fc89437-c8a4-46f2-8d73-1f5f1ef6a56c';
 
         return (
             <>
@@ -20,15 +23,28 @@ export class MainPage extends React.Component {
                         <IconContainer icon={ UuiPromoImage } />
                     </div>
                 </div>
+
                 <h1 style={{ display: "flex", alignItems: "center", flexDirection: 'column' }}>Reports</h1>
-                
                 <div style={{ display: "flex", alignItems: "center", flexDirection: 'column' }}>
-                    <PowerBiComponent reportId={reportId} groupId={groupId} typeEmbed={typeEmbed} defaultPage={defaultPage}
+                    <PowerBiComponent reportId={reportId} groupId={groupId} typeEmbed={typeReportEmbed} defaultPage={defaultPage1}
+                        defaultToken={defaultToken}>
+                    </PowerBiComponent>
+                </div>
+                <div style={{ height: "20px" }}></div>
+                <div style={{ display: "flex", alignItems: "center", flexDirection: 'column' }}>
+                    <PowerBiComponent reportId={reportId} groupId={groupId} typeEmbed={typeReportEmbed} defaultPage={defaultPage3}
                         defaultToken={defaultToken}>
                     </PowerBiComponent>
                 </div>
 
-                <div style={{ height: "100px" }}></div>
+                <h1 style={{ display: "flex", alignItems: "center", flexDirection: 'column' }}>Dashboard</h1>
+                <div style={{ display: "flex", alignItems: "center", flexDirection: 'column' }}>
+                    <PowerBiDashboardComponent reportId={DashboardId} typeEmbed={typeDashboardEmbed} 
+                        defaultToken={defaultToken}>
+                    </PowerBiDashboardComponent>
+                </div>
+
+                <div style={{ height: "120px" }}></div>
                 <Panel cx={ css.mainPanel } background='white'>
                     <RichTextView size="14">
                         <h3>Welcome to UUI template app</h3>
